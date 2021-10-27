2f1d0a83233ca306be9a3312fe0e8873c11443c6
Adam Becker <amkbecker@gmail.com>
2021-10-27 18:44:14 +0200

    Add OrderOptionCheckboxes, OrderOptionDropdown, OrderOptionIcons, OrderOptionNumber components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types';
 //import styles from './OrderForm.scss';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
+import setOrderOption from '../../../redux/orderRedux';
 
 const OrderForm = ({tripCost, options}) => (
     <Row>
+      {pricing.map(pricingOption => (
+      <Col md={4}>
+        <OrderOption 
+        key={pricingOption.id} 
+        currentValue={options[pricingOption.id]} 
+        setOrderOption={setOrderOption} 
+        {...pricingOption} />
+      </Col>
+      ))}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options}/>
       </Col>
@@ -15,6 +27,7 @@ const OrderForm = ({tripCost, options}) => (
 OrderForm.propTypes = {
     tripCost: PropTypes.string,
     options: PropTypes.object,
+    setOrderOption: PropTypes.func,
 };
 
 
